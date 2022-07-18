28a42827cf5836e79dd68bd9a30e36c9dee97762
vpavlishen <v.pavlishen@businessdecision.ru>
2022-07-19 00:10:29 +0300

    Model added

---
+++ src/models/model.py
@@ -0,0 +1,38 @@
+import torch
+import pytorch_lightning as pl
+
+
+class ModelSegmentationCT(pl.LightningModule):
+    def __init__(self, base_model, loss_function):
+        super().__init__()
+        self.model = base_model
+        self.loss_function = loss_function
+
+    def forward(self, data):
+        model_output = self.model(data)
+        return model_output
+
+    def training_step(self, batch):
+
+        data, target = batch
+
+        data, target = data.squeeze(0), target.squeeze(0)
+
+        prediction = self.model(data)
+        loss = self.loss_function(prediction, target)
+
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_index):
+        # this is the validation loop
+        data, target = batch
+
+        prediction = self.model(data)
+
+        vall_loss = self.loss_function(prediction, target)
+        self.log("val_loss", vall_loss)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
